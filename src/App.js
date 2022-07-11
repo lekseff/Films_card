a9737960904a1ea7cf52ed9e4e3a68886b43bfa9
Aleksey Zayachnikov <lekseff@gmail.com>
2022-07-12 00:26:36 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Stars from './components/Stars'
+import './App.css'
+import logo from './assets/images/logo.jpg'
 
 function App() {
+  const count = 5;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="cards">
+        <div className="card">
+            <div className="card__logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="card_description">
+                <div className="card__info">
+                    <h2 className="card__title">Аватар 2</h2>
+
+                    <Stars count={count} />
+
+                </div>
+                <div className="card__buttons">
+                    <button className="card__buy">999р</button>
+                    <a href="_">Подробнее...</a>
+                </div>                
+            </div>
+        </div>
+    </div>    
+  )
 }
 
 export default App;
---
+++ src/components/Stars.jsx
@@ -0,0 +1,20 @@
+import {v4} from 'uuid';
+import Star from './Star';
+
+function Stars(props) {
+  const {count} = props;
+  if (count <= 0 || count > 5) return;
+
+  const stars =  [];
+  for(let i = 1; i <= count; i++) {
+    stars.push(<Star key={v4()}/>)
+  }
+
+  return (
+    <ul className="card__rating">
+      {stars}
+    </ul>
+  )
+}
+
+export default Stars
